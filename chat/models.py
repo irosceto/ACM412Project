9c39993986e40132ba15ee482a310d51fdd6bc87
irosceto <irem2cetin@gmail.com>
2024-04-26 01:20:41 +0300

    Models have been created.

---
+++ chat/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+# Create your models here.
+
+class User(models.Model):
+    user_name = models.CharField(max_length=100)
+    user_email = models.EmailField(unique=True)
+    user_password = models.CharField(max_length=100)
+    sent_messages = models.ManyToManyField('Message', related_name='sender')
+    received_messages = models.ManyToManyField('Message', related_name='recipient')
+    chat_rooms = models.ManyToManyField('ChatRoom', related_name='members')
+
+
+class ChatRoom(models.Model):
+    name = models.CharField(max_length=100)
+    members = models.ManyToManyField(User, related_name='chat_rooms')
+
+
+class Message(models.Model):
+    chat_room = models.ForeignKey(ChatRoom, related_name='messages', on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
+    recipient = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+def __str__(self):
+    return f"{self.sender.user_name} -> {self.recipient.user_name}: {self.content}"

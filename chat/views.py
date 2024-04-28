1aa889b268ae5cb72e6fc4d14ccf9b7f57459acc
Aysenur1121 <nurero1121@gmail.com>
2024-04-28 17:27:43 +0300

    login ve signin viewları ve formları oluşturuldu ve url i eklendi

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import UserForm
 
-# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Kullanıcı kaydı başarılı, giriş sayfasına yönlendir
+    else:
+        form = UserForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+def login(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['user_name']
+            password = form.cleaned_data['user_password']
+            email = form.cleaned_data['user_email']
+            # Kullanıcıyı doğrula
+            user = authenticate(username=user_name, password=user_password, email=user_email)
+            if user is not None:
+                # Kullanıcı doğrulandıysa, giriş yap
+                login(request, user)
+                return redirect('home')  # Başarılı girişten sonra yönlendirilecek sayfa
+                # Kullanıcının tarayıcısına çerez ekle
+                response = HttpResponse("Başarılı giriş!")
+                response.set_cookie('username', username)
+                return response
+            else:
+                return HttpResponse("Geçersiz kullanıcı adı veya parola!")
+    else:
+        form = UserForm()
+    return render(request, 'login.html', {'form': form})

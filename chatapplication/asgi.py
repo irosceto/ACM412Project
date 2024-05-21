db31baef6ade8b7e6063aa7749edb9b523781855
irosceto <irem2cetin@gmail.com>
2024-05-21 17:40:13 +0300

    Birsürü değişiklik yapıldı -irem çetin

--- chatapplication/asgi.py
+++ chatapplication/asgi.py
@@ -8,15 +8,29 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
-
+import django
+import chatapplication.routing
+from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application
+#import chatapplication.routing
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from django.core.asgi import get_asgi_application
+
 
 from django.urls import path
 
-from channels.auth import AuthMiddlewareStack
+import chatapplication
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatapplication.settings')
-
+django.setup()
 django_asgi_app = get_asgi_application()
+
+
+application = ProtocolTypeRouter({
+    'http': django_asgi_app,
+    'websocket':
+        AuthMiddlewareStack(
+            URLRouter(chatapplication.routing.websocket_urlpatterns)
+        )
+
+})

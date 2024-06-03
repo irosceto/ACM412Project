af2d8a0e844178ddbf2d96d8a14178c6ebdf8ae2
irosceto <irem2cetin@gmail.com>
2024-06-03 16:25:01 +0300

    Birsürü değişiklik yapıldı -irem çetin

--- chatapplication/urls.py
+++ chatapplication/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 
@@ -55,4 +57,5 @@ urlpatterns = [
     path('api/chat_rooms/<int:room_id>', room_detail, name='room_detail'),
     path('chat_rooms/<int:chat_room_id>', RoomMembersView.as_view(), name='chat_room_users'),
 ]
-
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

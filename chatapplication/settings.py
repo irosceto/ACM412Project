cfe5a5aabde44ea4c7bbfcfc7294103f9d6c4bb9
Aysenur1121 <nurero1121@gmail.com>
2024-06-13 17:38:36 +0300

    cors

--- chatapplication/settings.py
+++ chatapplication/settings.py
@@ -178,6 +178,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:8000",
+    "https://my-django-frontend-1.onrender.com"
     # Diğer izin verilen kökenler buraya eklenebilir
 ]
 
